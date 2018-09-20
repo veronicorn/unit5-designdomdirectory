c996e216b554ffd83438d7ddef6146d1f1f5cde2
veronicorn <vronicalee@gmail.com>
2018-09-19 21:25:27 -0400

    fixing codes up

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const employeeList = [
+const state = {
+    employeeList: [
     {
         name: 'Jan',
         officeNum: 1,
@@ -39,477 +40,125 @@ const employeeList = [
         officeNum: 345,
         phoneNum: '222-789-5231'
     }
-];
+    ],
+    activeList: []
+}
 
-let databaseFunction = '';
-
-const runFunction = function (e) {
-    e.preventDefault();
-    switch (databaseFunction){
+const runFunction = function () {
+    let e = state.employeeList;
+    switch (databaseFunction) {
         case 'print':
-            render('print');
-            employeeList.forEach(e => renderEmployee(e));
+            $('.displaycontent').html('');
+            e.forEach(e => render(e));
             break;
+        case 'verify':
+            $('.displaycontent').html('');
+            let verifyInput = $('.verifyInput').val();
+            if (e.find(e => e.name.toLowerCase() === verifyInput.toLowerCase())) {
+                render(e.find(e => e.name.toLowerCase() === verifyInput.toLowerCase()));
+            } else {
+                $('.displaycontent').html(`Employee Not Found`)
+            }
+            break;
+        case 'lookup':
+            $('.displaycontent').html('');
+            let lookupInput = $('.lookupInput').val();
+            if (e.find(e => e.name.toLowerCase () === lookupInput.toLowerCase())) {
+                render(e.find(e => e.name.toLowerCase() === lookupInput.toLowerCase()));
+            } else {
+                $('.displaycontent').html(`Employee Not Found`)
+            break;
+            }
+        case 'contains':
+            $('.displaycontent').html('');
+            let containEmployee = '';
+            let containsInput = $('.containsInput').val();
+            for (let i = 0; i < e.length; i++) {
+                if (e[i].name.toLowerCase().includes(containsInput.toLowerCase())) {
+                    containEmployee = `Name: ${e[i].name} Office Number: ${e[i].officeNum} Phone Number: ${e[i].phoneNum}`;
+                    $('.displaycontent').append(`${containEmployee}`);
+                }
+            }
+                if ($('.displaycontent').html() === '') {
+                    $('.displaycontent').append(`No Employees Found<`)
+                } 
+                break;
+                
+        }
     }
-}
 
-//     if (databaseFunction === 'print') {
-//         let htmlStr = '';
-//         for (let i = 0; i < employeeList.length; i++) {
-//             htmlStr += `<div class="print"><p> ${employeeList[i].name} </p>`;
-//             htmlStr += `<p> ${employeeList[i].officeNum} </p>`;
-//             htmlStr += `<p> ${employeeList[i].phoneNum} </p>`;
-//             htmlStr += `<p> ----- </p></div>`;
-//         }
-//         render(htmlStr);
-//     } else if (databaseFunction === 'verify') {
-//         let htmlStr = '<div class="print"><p> Employee Not Found </p></div>';
-//         for (let i = 0; i < employeeList.length; i++) {
-//             if (employeeList[i].name.toLowerCase() === $('#input').val().toLowerCase()) {
-//                 htmlStr = '<div class="print"><p> Employee Found </p></div>';
-//             }
-//         }
-//         render(htmlStr);
-//     }
-//     else if (databaseFunction === 'lookup') {
-//         let htmlStr = '';
-//         let lookup = false;
-//         for (let i = 0; i < employeeList.length; i++) {
-//             if (employeeList[i].name.toLowerCase() === $('#input').val().toLowerCase()) {
-//                 htmlStr += `<div class="print"><p> ${employeeList[i].name} </p>`;
-//                 htmlStr += `<p> ${employeeList[i].officeNum} </p>`;
-//                 htmlStr += `<p> ${employeeList[i].phoneNum} </p></div>`;
-//                 lookup = true;
-//             }
-//         }
-//         if (lookup === false) {
-//             htmlStr += `<p>Employee Not Found</p>`;
-//         }
-//         render(htmlStr);
-//     } else if (databaseFunction === 'contains') {
-//         let htmlStr = '';
-//         let containsEmployee = false
-//         for (let i = 0; i < employeeList.length; i++) {
-//             if (employeeList[i].name.toLowerCase().includes($('#input').val().toLowerCase())) {
-//                 htmlStr += `<div class="print"><p> ${employeeList[i].name} </p>`;
-//                 htmlStr += `<p> ${employeeList[i].officeNum} </p>`;
-//                 htmlStr += `<p> ${employeeList[i].phoneNum} </p>`;
-//                 htmlStr += `<p> ----- </p></div>`;
-//                 containsEmployee = true
-//             }
-//         }
-//         if (containsEmployee === false) {
-//             htmlStr += `<p>No Employee Match</p>`;
-//         }
-//         render(htmlStr);
-//     } else if (databaseFunction === 'update') {
-//         let htmlStr = '';
-//         for (let i = 0; i < employeeList.length; i++) {
-//             if (employeeList[i].name.toLowerCase() === $('#input').val().toLowerCase()) {
-//                 employeeList[i].officeNum = $('#office').val();
-//                 employeeList[i].phoneNum = $('#phone').val();
-//                 htmlStr += `<div class="print"><p> ${employeeList[i].name} </p>`;
-//                 htmlStr += `<p> ${employeeList[i].officeNum} </p>`;
-//                 htmlStr += `<p> ${employeeList[i].phoneNum} </p></div>`;
-//             }
-//         }
-//         render(htmlStr);
-//     } else if (databaseFunction === 'add') {
-//         let htmlStr = '';
-//         const newEmployee = {
-//             name: $('#input').val(),
-//             officeNum: $('#office').val(),
-//             phoneNum: $('#phone').val()
-//         }
-
-//         employeeList.push(newEmployee);
-
-//         for (let i = 0; i < employeeList.length; i++) {
-//             htmlStr += `<div class="print"><p> ${employeeList[i].name} </p>`;
-//             htmlStr += `<p> ${employeeList[i].officeNum} </p>`;
-//             htmlStr += `<p> ${employeeList[i].phoneNum} </p>`;
-//             htmlStr += `<p> ----- </p></div>`;
-//         }
-//         render(htmlStr);
-//     } else if (databaseFunction === 'delete') {
-//         let htmlStr = '';
-//         for (let i = 0; i < employeeList.length; i++) {
-//             if (employeeList[i].name.toLowerCase() === $('#input').val().toLowerCase()) {
-//                 employeeList.splice(i, 1);
-//             }
-//         }
-//         for (let i = 0; i < employeeList.length; i++) {
-//             htmlStr += `<div class="print"><p> ${employeeList[i].name} </p>`;
-//             htmlStr += `<p> ${employeeList[i].officeNum} </p>`;
-//             htmlStr += `<p> ${employeeList[i].phoneNum} </p>`;
-//             htmlStr += `<p> ----- </p></div>`;
-//         }
-//         render(htmlStr);
-//     } else if (databaseFunction === 'alphabetize') {
-//         let htmlStr = '';
-//         employeeList.sort(function (a, b) {
-//             var nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase()
-//             if (nameA < nameB)
-//                 return -1
-//             if (nameA > nameB)
-//                 return 1
-//             return 0
-//         })
-//         for (let i = 0; i < employeeList.length; i++) {
-//             htmlStr += `<div class="print"><p> ${employeeList[i].name} </p>`;
-//             htmlStr += `<p> ${employeeList[i].officeNum} </p>`;
-//             htmlStr += `<p> ${employeeList[i].phoneNum} </p>`;
-//             htmlStr += `<p> ----- </p></div>`;
-//         }
-//         render(htmlStr);
-//     }
-// }
-
-const render = function (htmlStr) {
-    $('#render').html(htmlStr);
-}
-
-const hideForm = function () {
-    $('form').removeClass('show')
-}
-
-const showForm = function () {
-    $('form').addClass('show');
-}
-
-const addInput = function () {
-    $('#input, #submit').addClass('show');
-}
-
-const hideInput = function () {
-    $('#input, #submit').removeClass('show');
-}
-
-const addFields = function () {
-    $('#office, #phone').addClass('show');
-}
-
-const removeFields = function () {
-    $('#office, #phone').removeClass('show');
-}
-
-const hidePrint = function () {
-    $('.print').addClass('hide');
-}
-
-const print = function (event) {
+const print = function () {
     databaseFunction = 'print';
-    runFunction(event);
-    removeFields();
-    hideInput();
-    hideForm();
+    $('.welcome').addClass('hide');
+    $('.lookupPage').addClass('hide');
+    $('.verifyPage').addClass('hide');
+    $('.printPage').addClass('hide');
+    $('.containsPage').addClass('hide');
+    $('.updatePage').addClass('hide');
+    $('.addPage').addClass('hide');
+    $('.deletePage').addClass('hide');
+    runFunction();
 }
 
-const verify = function (event) {
+const verifyPage = function () {
+    $('.welcome').addClass('hide');
+    $('.displaycontent').html('');
+    $('.verifyPage').removeClass('hide');
+    $('.printPage').addClass('hide');
+    $('.lookupPage').addClass('hide');
+    $('.containsPage').addClass('hide');
+    $('.updatePage').addClass('hide');
+    $('.addPage').addClass('hide');
+    $('.deletePage').addClass('hide');
+}
+
+const verify = function () {
     databaseFunction = 'verify';
-    hidePrint();
-    addInput();
-    removeFields();
-    showForm();
+    runFunction();
 }
 
-const lookup = function (event) {
+const lookupPage = function () {
+    $('.welcome').addClass('hide');
+    $('.displaycontent').html('');
+    $('.lookupPage').removeClass('hide');
+    $('.verifyPage').addClass('hide');
+    $('.printPage').addClass('hide');
+    $('.containsPage').addClass('hide');
+    $('.updatePage').addClass('hide');
+    $('.addPage').addClass('hide');
+    $('.deletePage').addClass('hide');
+}
+
+const lookup = function () {
     databaseFunction = 'lookup';
-    hidePrint();
-    addInput();
-    removeFields();
-    showForm();
+    runFunction();
+}
+
+const containsPage = function () {
+    $('.welcome').addClass('hide');
+    $('.displaycontent').html('');
+    $('.containsPage').removeClass('hide');
+    $('.verifyPage').addClass('hide');
+    $('.printPage').addClass('hide');
+    $('.lookupPage').addClass('hide');
+    $('.updatePage').addClass('hide');
+    $('.addPage').addClass('hide');
+    $('.deletePage').addClass('hide');
 }
 
 const contains = function () {
     databaseFunction = 'contains';
-    hidePrint();
-    addInput();
-    removeFields();
-    showForm();
+    runFunction();
 }
 
-const update = function () {
-    databaseFunction = 'update';
-    hidePrint();
-    addInput();
-    addFields();
-    showForm();
-}
-
-const add = function () {
-    databaseFunction = 'add';
-    hidePrint();
-    addInput();
-    addFields();
-    showForm();
-}
-
-const remove = function () {
-    databaseFunction = 'delete';
-    hidePrint();
-    addInput();
-    removeFields();
-    showForm();
-}
-
-const alphabetize = function (event) {
-    databaseFunction = 'alphabetize';
-    runFunction(event);
-    removeFields();
-    hideInput();
-    hideForm();
+const render = function (e) {
+    $('.displaycontent').append(`<div class="box"><h5>Name: ${e.name}</h5><h5>Office Number: ${e.officeNum}</h5><h5>Phone Number: ${e.phoneNum}</h5> </div>`);
 }
 
 $('#print').on('click', print);
-$('#verify').on('click', verify);
-$('#lookup').on('click', lookup);
-$('#contains').on('click', contains);
-$('#update').on('click', update);
-$('#add').on('click', add);
-$('#delete').on('click', remove);
-$('button').on('click', runFunction);
-
-
-
-// let command = '';
-
-// const runCommand = function (e) {
-//     e.preventDefault();
-//     switch (command) {
-//         case 'print':
-//         console.log('printing');
-//             // employeeList.forEach(e => renderEmployee(e));
-//             break;
-//         case 'lookup':
-//         console.log('verifying');
-//             break;
-
-
-//         // const render = function (htmlString) {
-//         // $('employeeData').html(htmlString);
-//     }
-
-
-
-// const print = function(){
-//     command = 'print';
-//     $('input').removeClass('show');
-// }
-
-// const verify = function(){
-//     command = 'verify';
-//     $('input').addClass('show');
-// }
-
-
-// $('#print').on('click', print);
-// $('#verify').on('click', verify);
-// $('#submit').on('click', runCommand);
-
-// //print--------------------------------- 
-// const print = function (event){
-//     event.preventDefault();
-//     $('#boxOne').empty();
-//     for (let i = 0; i < employeeList.length; i++){
-//         $('#boxOne').append(
-//             `<p>${employeeList[i].name}</p>
-//             <br>${employeeList[i].officeNum}</br>
-//             <br>${employeeList[i].phoneNum}</br>
-//             <br></br>`
-//         );
-//     }
-// }
-
-// $('#print').on('click', print);
-
-
-// //verify-----------------------was working fine Sunday, now input box not appearing!
-// const verify = function (event){
-//     event.preventDefault();
-//     $('#boxOne').empty();
-//     $('#boxOne').append(`<br><input type='text' id='userVerify'></br>`);
-//     $('#boxOne').append(`<button id="submitVerify">Verify</button>`);
-
-//     $('#submitVerify').on('click', userVerify);
-// }
-
-// const userVerify = function(event){
-//     event.preventDefault();
-//     if(employeeList.some(e => e.name.toLowerCase() === $('#userVerify').val().toLowerCase()) === true){
-//         $('#boxTwo').append(`<div>Employee Found</div>`);
-//     } else {
-//         $('#boxTwo').append('<div>Employee Not Found</div>');
-//     }    
-// }
-
-// $('#verify').on('click', verify);
-
-
-// //lookup----------------------------------not working yet
-// const lookup = function (event){
-//     event.preventDefault();
-//     $('#boxOne').empty();
-//     $('#boxOne').append(
-//         `<br><input id='userLookup' type='text' placeholder='Employee Name' autocomplete="off" />
-//         <button id='submitLookup'>Lookup</button>
-//         </br>`
-//     );
-
-//     $('#submitLookup').on('click', userLookup);
-// }
-
-// const userLookup = function(event){
-//     event.preventDefault();
-//     for (i = 0; i < employeeList.length; i++){
-//         if ($('#userLookup') === employeeList[i].name.toLowerCase() === true){
-//             $('#boxTwo').append(
-//                 `<p>${employeeList[i].name}</p>
-//                 <br>${employeeList[i].officeNum}</br>
-//                 <br>${employeeList[i].phoneNum}</br>
-//                 <br></br>`
-//             )
-//         } else {
-//             $('#boxTwo').append('<div>Employee Not Found</div>');
-//         }
-//     }
-// }
-
-// $('#lookup').on('click', lookup);
-
-
-//contains----------------------------------Act30code
-// else if (command.toLowerCase() === 'contains'){
-
-// const inputString = prompt('enter a string to list employees');
-
-// for (i = 0; i < employeeList.length; i++){
-//     if (employeeList[i].name.toLowerCase().includes(inputString)){
-//     render(employeeList[i].name);
-//     render(employeeList[i].officeNum);
-//     render(employeeList[i].phoneNum); 
-//     }
-// }
-// }
-
-
-//update----------------------------------Act30code
-// else if (command.toLowerCase() === 'update'){
-
-// const updateName = prompt('enter an employee name to update');
-// const updateField = prompt('enter officeNum or phoneNum to update');
-// const newValue = prompt('enter a value to update');
-
-// for (let i = 0; i < employeeList.length; i++){
-//     if (updateName === employeeList[i].name.toLowerCase()){
-//     if (updateField === "officeNum"){
-//         employeeList[i].officeNum = newValue;
-//     }
-//     else if (updateField === "phoneNum"){
-//         employeeList[i].phoneNum = newValue;
-//     }
-//     render(employeeList[i].name);
-//     render(employeeList[i].officeNum);
-//     render(employeeList[i].phoneNum); 
-//     }
-// }
-// }
-
-
-//add----------------------------------incomplete/not working
-// const addInput = function(event){
-//     event.preventDefault();
-//     $('#boxOne').empty();
-//     $('#boxOne').append(
-//         `<br>Name <input id="userAddName" type="text" placeholder="Type New Employee Name" autocomplete="off" /></br>
-//         <br>Office Number <input id="userAddOffice" type="text" placeholder="Type New Office Number" autocomplete="off" /></br>
-//         <br>Phone Number <input id="userAddPhone" type="text" placeholder="Type New Phone Number" autocomplete="off" /></br>
-//         <br><button id="submitAdd">Add</button></br>
-//         </br>`
-//     );
-
-//     $('#submitAdd').on('click', userAdd);
-// }
-
-
-//delete----------------------------------Act30code
-// else if (command.toLowerCase() === 'delete'){
-
-// const deleteEmployeeName = prompt('enter the name of an employee to delete');
-// const index = -1;
-
-// for (i = 0; i < employeeList.length; i++){
-//     if (employeeList[i].name.toLowerCase() === deleteEmployeeName){
-//     index = i;
-//     }
-// }
-
-// employeeList.splice(index, 1);
-//     for (i = 0; i < employeeList.length; i++){
-//     render(employeeList[i].name);
-//     render(employeeList[i].officeNum);
-//     render(employeeList[i].phoneNum);
-//     }
-// }
-
-
-
-
-
-
-
-// CJ's code from help session
-
-// let command = '';
-
-// const runCommand = function (event) {
-//     event.preventDefault();
-//     switch (command) {
-//         case 'print':
-//             let htmlString = '';
-//             for (let i = 0; i < employeeList.length; i++) {
-//                 htmlString += `<p>${employeeList[i].name}</p>`;
-//                 htmlString += `<br>${employeeList[i].officeNum}</br>`;
-//                 htmlString += `<br>${employeeList[i].phoneNum}</br>`;
-//             }  
-//             render(htmlString);
-//         break;
-//         case 'verify':
-//                 $('#boxOne').append(
-//                     `<br><input id="userVerify" type="text" placeholder="Employee Name" autocomplete="off" />
-//         <button id="submitVerify">Verify</button>
-//         </br>`
-//                 );
-
-//                 $('#submitVerify').on('click', userVerify);
-//             }
-
-
-//             const message = false;
-
-//             for (i = 0; i < employeeList.length; i++){
-//             if (verify === employeeList[i].name.toLowerCase()){
-//             message = true;
-//             }
-//             }
-
-//             render(message);
-//         break;
-//     }
-// }
-
-// const render = function (htmlString) {
-//     $('employeeData').html(htmlString);
-// }
-
-// const print = function (event) {
-//     event.preventDefault();
-//     command = 'print';
-//     $('input').removeClass('show');
-// }
-
-// const verify = function (event) {
-//     event.preventDefault();
-//     command = 'verify';
-//     $('input').addClass('show');
-// }
+$('#verify').on('click', verifyPage);
+$('#verifyBtn').on('click', verify);
+$('#lookup').on('click', lookupPage);
+$('#lookupBtn').on('click', lookup);
+$('#contains').on('click', containsPage);
+$('#containsBtn').on('click', contains);
